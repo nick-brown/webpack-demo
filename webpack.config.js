d8b66c226f0d77373215922fe322d40856146454
Nick Brown <nick@nickbrown.io>
2016-07-22 02:00:06 -0700

    Add config merge and validator plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of entries.
   // We'll be using the latter form given it's
   // convenient with more complex configurations.
@@ -23,3 +25,13 @@ module.exports = {
     })
   ]
 };
+
+var config;
+
+if(process.env.npm_lifecycle_event === 'build') {
+  config = merge(common, {});
+} else {
+  config = merge(common, {});
+}
+
+module.exports = validate(config);
